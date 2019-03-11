dece35dd11c4beea6ec1e30c049bc106b6902d78
Yasser Mohammad <yasserfarouk@gmail.com>
2019-03-11 14:33:37 +0900

    version bumping

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     # test_suite='negmas/tests',
     tests_require=test_requirements,
     url='https://github.com/yasserfarouk/negmas',
-    version='0.1.28',
+    version='0.1.29',
     zip_safe=False,
 )
