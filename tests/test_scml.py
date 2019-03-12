a98d38ad68f9d89654a6ce90a406e7b7fb473e82
Yasser Mohammad <yasserfarouk@gmail.com>
2019-03-12 21:17:46 +0900

    preparation for rtd

--- tests/test_scml.py
+++ tests/test_scml.py
@@ -4,13 +4,13 @@ from negmas.apps.scml.utils import anac2019_world
 
 
 @pytest.mark.parametrize('n_steps,consumption_horizon'
-    , [(10, 10), (60, 10)], ids=['short', 'default'])
+    , [(20, 15), (200, 15)], ids=['short', 'default'])
 def test_anac2019(n_steps, consumption_horizon):
     world = anac2019_world(n_steps=n_steps, consumption_horizon=consumption_horizon)
     world.run()
     assert world.business_size > 0.0
-    assert world.breach_rate < 0.9
-    assert world.agreement_rate > 0.1
+    assert world.breach_rate < 0.99
+    assert world.agreement_rate > 0.01
 
 
 if __name__ == '__main__':
