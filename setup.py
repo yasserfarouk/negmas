e1c120194d13d060c71dce877e832ad554a92144
Yasser Mohammad <yasserfarouk@gmail.com>
2019-03-28 11:54:14 +0900

    version bumping for readthedocs

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     name='negmas',  # Required
     # https://www.python.org/dev/peps/pep-0440/
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.37',  # Required
+    version='0.1.38',  # Required
     # https://packaging.python.org/specifications/core-metadata/#summary
     description="NEGotiations Managed by Agent Simulations",  # Required
     # https://packaging.python.org/specifications/core-metadata/#description-optional
