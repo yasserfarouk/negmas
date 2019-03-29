566ab7d8071de68d3540b03e9725db820a2df177
Yasser Mohammad <yasserfarouk@gmail.com>
2019-03-29 14:16:20 +0900

    reading jar locations for both jnegmas and genius-bridge from config file

--- negmas/__init__.py
+++ negmas/__init__.py
@@ -4,6 +4,25 @@ __author__ = """Yasser Mohammad"""
 __email__ = 'yasserfarouk@gmail.com'
 __version__ = '0.1.44'
 
+import json
+import pathlib
+
+
+NEGMAS_CONFIG = {}
+"""Global configuration parameters for NegMAS"""
+CONFIG_KEY_JNEGMAS_JAR = 'jnegmas_jar'
+"""Key name for the JNegMAS jar in `NEGMAS_CONFIG`"""
+CONFIG_KEY_GENIUS_BRIDGE_JAR = 'genius_bridge_jar'
+"""Key name for the Genius bridge jar in `NEGMAS_CONFIG`"""
+
+
+# loading config file if any
+__conf_path = pathlib.Path('~/negmas/config.json').expanduser().absolute()
+
+if __conf_path.exists():
+    with open(__conf_path, 'r') as f:
+        NEGMAS_CONFIG = json.load(f)
+
 from .common import *
 from .outcomes import *
 from .utilities import *
