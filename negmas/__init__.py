00b48c1bebd36d3f8de6fc7ca502c18840d9ce57
Yasser Mohammad <yasserfarouk@gmail.com>
2019-03-29 15:26:41 +0900

    Bump version: 0.1.44 → 0.1.45

--- negmas/__init__.py
+++ negmas/__init__.py
@@ -2,7 +2,7 @@
 """A framework for conducting multi-strand multilateral asynchronous negotiations on multiple issues."""
 __author__ = """Yasser Mohammad"""
 __email__ = 'yasserfarouk@gmail.com'
-__version__ = '__version__ = '0.1.44''
+__version__ = '__version__ = '0.1.45''
 
 import json
 import pathlib
