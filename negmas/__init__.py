4125c7c687c2a46a54660506898b8015d3cb58bb
Yasser Mohammad <yasserfarouk@gmail.com>
2019-03-29 10:14:59 +0900

    version bumping

--- negmas/__init__.py
+++ negmas/__init__.py
@@ -2,7 +2,7 @@
 """A framework for conducting multi-strand multilateral asynchronous negotiations on multiple issues."""
 __author__ = """Yasser Mohammad"""
 __email__ = 'yasserfarouk@gmail.com'
-__version__ = '0.1.40'
+__version__ = '0.1.41'
 
 from .common import *
 from .outcomes import *
