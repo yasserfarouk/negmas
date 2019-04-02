751751ef29e628eb7f3fc6a6920260fe95b68163
Yasser Mohammad <yasserfarouk@gmail.com>
2019-04-02 20:47:23 +0900

    official release for ANAC 2019 league (up to patches)

--- negmas/__init__.py
+++ negmas/__init__.py
@@ -2,7 +2,7 @@
 """A framework for conducting multi-strand multilateral asynchronous negotiations on multiple issues."""
 __author__ = """Yasser Mohammad"""
 __email__ = 'yasserfarouk@gmail.com'
-__version__ = '0.1.48'
+__version__ = '0.2.0'
 
 import json
 import pathlib
