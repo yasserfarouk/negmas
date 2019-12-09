c5aa9c6958f2685672e546d7876e027251a46f32
Yasser Mohammad <yasserfarouk@gmail.com>
2019-12-09 14:26:04 +0900

    black formatting

--- negmas/gui/app.py
+++ negmas/gui/app.py
@@ -10,6 +10,7 @@ from typing import Type
 
 from negmas import NamedObject
 from negmas.visualizers import *
+
 # 'https://codepen.io/chriddyp/pen/bWLwgP.css',
 external_style_sheets = [dbc.themes.CERULEAN]
 
@@ -43,22 +44,25 @@ def cli(debug=True):
                 html.P("Run a new component", className="card-text"),
                 dbc.Select(
                     options=[
-                        {'label': 'SCMLWorld', 'value': 'negmas.apps.scml.SCMLWorld'},
-                        {'label': 'World', 'value': 'negmas.situated.World'},
-                        {'label': 'Negotiation', 'value': 'negmas.mechanisms.Mechanism'},
+                        {"label": "SCMLWorld", "value": "negmas.apps.scml.SCMLWorld"},
+                        {"label": "World", "value": "negmas.situated.World"},
+                        {
+                            "label": "Negotiation",
+                            "value": "negmas.mechanisms.Mechanism",
+                        },
                     ],
-                    value='negmas.apps.scml.SCMLWorld',
+                    value="negmas.apps.scml.SCMLWorld",
                     id="new-type",
                     className="mt-1",
                 ),
                 dbc.Input(
-                    placeholder='Config file path ...',
-                    type='file',
-                    value='',
+                    placeholder="Config file path ...",
+                    type="file",
+                    value="",
                     id="new-config-path",
                     className="mt-1",
                 ),
-                dbc.Button("Run")
+                dbc.Button("Run"),
             ]
         ),
         className="mt-3",
@@ -69,28 +73,26 @@ def cli(debug=True):
             [
                 html.P("Monitor a component", className="card-text"),
                 dbc.Input(
-                    placeholder='Checkpoint folder ...',
-                    type='file',
-                    value='',
+                    placeholder="Checkpoint folder ...",
+                    type="file",
+                    value="",
                     id="checkpoint-folder",
                     className="mt-1",
                 ),
                 dbc.Input(
-                    placeholder='[Optional] component ID',
-                    type='text',
-                    value='',
+                    placeholder="[Optional] component ID",
+                    type="text",
+                    value="",
                     id="checkpoint-id",
                     className="mt-1",
                 ),
                 dbc.Checklist(
-                    options=[
-                        {'label': 'Watch Folder', 'value': 'watch'},
-                    ],
+                    options=[{"label": "Watch Folder", "value": "watch"}],
                     value=[],
                     id="checkpoint-options",
                     className="mt-1",
                 ),
-                dbc.Button("Monitor")
+                dbc.Button("Monitor"),
             ]
         ),
         className="mt-3",
@@ -104,44 +106,39 @@ def cli(debug=True):
                         [
                             dbc.Container(
                                 [
-                                    dbc.Row(
-                                        [
-                                            html.H2("Load"),
-                                        ],
-                                    ),
+                                    dbc.Row([html.H2("Load")]),
                                     dbc.Row(
                                         [
                                             dbc.Tabs(
                                                 [
-                                                    dbc.Tab(run_online, label="Tab 1", tab_id="online"),
-                                                    dbc.Tab(run_offline, label="Tab 2", tab_id="offline"),
-
+                                                    dbc.Tab(
+                                                        run_online,
+                                                        label="Tab 1",
+                                                        tab_id="online",
+                                                    ),
+                                                    dbc.Tab(
+                                                        run_offline,
+                                                        label="Tab 2",
+                                                        tab_id="offline",
+                                                    ),
                                                 ],
                                                 id="open-tabs",
                                                 active_tab="online",
-                                            ),
-                                        ],
+                                            )
+                                        ]
                                     ),
-                                    dbc.Row(
-                                        [
-                                            html.H2("Children"),
-                                        ],
-                                    ),
-                                    dbc.Row(
-                                        [
-                                            html.P("Children will appear here")
-                                        ],
-                                    ),
+                                    dbc.Row([html.H2("Children")]),
+                                    dbc.Row([html.P("Children will appear here")]),
                                 ],
                                 className="mt-0",
-                            ),
+                            )
                         ],
                         md=3,
                     ),
                     dbc.Col(
                         [
                             html.H2("Showing xyz (ID fdfdsf)", id="basic"),
-                            html.Div(id="main_widget")
+                            html.Div(id="main_widget"),
                         ],
                         md=9,
                     ),
@@ -158,5 +155,5 @@ def cli(debug=True):
 if __name__ == "__main__":
     debug = False
     if len(sys.argv) > 1 and sys.argv[1] in ("debug", "--debug", "-d"):
-        debug=True
+        debug = True
     cli(debug=debug)
