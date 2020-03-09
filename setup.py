7e62fc42ea161c698752f6d85fbd51c1d4c4ac1f
Yasser Mohammad <yasserfarouk@gmail.com>
2020-03-09 22:53:37 +0900

    v0.6.6

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     name="negmas",  # Required
     # https://www.python.org/dev/peps/pep-0440/
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.6.5",  # Required
+    version="0.6.6",  # Required
     # https://packaging.python.org/specifications/core-metadata/#summary
     description="NEGotiations Managed by Agent Simulations",  # Required
     # https://packaging.python.org/specifications/core-metadata/#description-optional
