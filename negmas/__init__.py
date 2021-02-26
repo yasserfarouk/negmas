2bd9234273f156923068575a82bfc2b555099796
Yasser Mohammad <yasserfarouk@gmail.com>
2021-02-26 16:38:36 +0900

    v 0.7.3

--- negmas/__init__.py
+++ negmas/__init__.py
@@ -2,7 +2,7 @@
 """A framework for conducting multi-strand multilateral asynchronous negotiations on multiple issues."""
 __author__ = """Yasser Mohammad"""
 __email__ = "yasserfarouk@gmail.com"
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 from .common import *
 from .config import *
