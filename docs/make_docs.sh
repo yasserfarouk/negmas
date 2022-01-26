6d9e55639ab64fd320f94451cd0eb9cc51da2c76
Yasser Mohammad <yasserfarouk@gmail.com>
2022-01-26 13:38:02 +0900

    avoid documnetaiton construction failure

--- docs/make_docs.sh
+++ docs/make_docs.sh
@@ -8,3 +8,4 @@ mkdir -p "./_build/html/tutorials/figs"
 [ -d "../notebooks/figs" ] && cp ../notebooks/figs/*.* "./_build/html/figs/"
 [ -d "../notebooks/figs" ] && cp ../notebooks/figs/*.* "./_build/html/tutorials/figs"
 # open _build/html/index.html
+echo "done!!"
