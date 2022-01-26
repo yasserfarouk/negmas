96b472ae637a8420050db19352098ac94ec27cc3
Yasser Mohammad <yasserfarouk@gmail.com>
2022-01-26 12:53:50 +0900

    doc update

--- docs/make_docs.sh
+++ docs/make_docs.sh
@@ -2,6 +2,9 @@
 ./update.sh
 make html
 mkdir -p "./_build/html/figs"
-cp "../notebooks/figs/*.*" "./_build/html/figs/"
-cp "../notebooks/figs/*.*" "./_build/html/tutorials/figs"
+mkdir -p "./_build/html/tutorials/figs"
+[ -d "./figs" ] && cp "./figs/*.*" "./_build/html/figs/"
+[ -d "./figs" ] && cp "./figs/*.*" "./_build/html/tutorials/figs"
+[ -d "../notebooks/figs" ] && cp "../notebooks/figs/*.*" "./_build/html/figs/"
+[ -d "../notebooks/figs" ] && cp "../notebooks/figs/*.*" "./_build/html/tutorials/figs"
 # open _build/html/index.html
