e7080a687734bfd44581f8927d5f888abd55e8dc
Yasser Mohammad <yasserfarouk@gmail.com>
2022-03-10 08:34:12 +0900

    v0.9.2

--- negmas/__init__.py
+++ negmas/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __author__ = """Yasser Mohammad"""
 __email__ = "yasserfarouk@gmail.com"
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 
 
 from .common import *
