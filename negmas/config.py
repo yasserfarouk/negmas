ac5ae728dcd880cd63651d8b5d71343820d2ef03
Yasser Mohammad <yasserfarouk@gmail.com>
2023-01-15 11:27:27 +0900

    CLI update (allowing adapters)

--- negmas/config.py
+++ negmas/config.py
@@ -14,8 +14,8 @@ __all__ = [
 
 LOCAL_NEGMAS_CONFIG_FILENAME = "negmasconf.json"
 
-NEGMAS_DEFAULT_PATH = environ.get(
-    "NEGMAS_DEFAULT_PATH", Path.home() / "negmas" / "config.json"
+NEGMAS_DEFAULT_PATH = Path(
+    environ.get("NEGMAS_DEFAULT_PATH", Path.home() / "negmas" / "config.json")
 )
 """Default path for NegMAS configurations"""
 
@@ -26,8 +26,10 @@ CONFIG_KEY_GENIUS_BRIDGE_JAR = "genius_bridge_jar"
 """Key name for the Genius bridge jar in `NEGMAS_CONFIG`"""
 
 NEGMAS_CONFIG = {
-    CONFIG_KEY_JNEGMAS_JAR: f"{NEGMAS_DEFAULT_PATH}/{CONFIG_KEY_JNEGMAS_JAR}",
-    CONFIG_KEY_GENIUS_BRIDGE_JAR: f"{NEGMAS_DEFAULT_PATH}/{CONFIG_KEY_GENIUS_BRIDGE_JAR}",
+    CONFIG_KEY_JNEGMAS_JAR: str(NEGMAS_DEFAULT_PATH.parent / "files" / "jnegmas.jar"),
+    CONFIG_KEY_GENIUS_BRIDGE_JAR: str(
+        NEGMAS_DEFAULT_PATH.parent / "files" / "geniusbridge.jar"
+    ),
 }
 
 # loading config file if any
--- negmas/scripts/negotiate.py
+++ negmas/scripts/negotiate.py
@@ -5,7 +5,9 @@ from time import perf_counter
 
 import matplotlib
 import typer
+from matplotlib import functools
 from matplotlib import pyplot as plt
+from pandas.core.window.numba_ import Callable
 from rich import print
 from stringcase import titlecase
 
@@ -52,13 +54,37 @@ def get_protocol(name: str) -> type[Mechanism]:
     return get_class(name)
 
 
-def get_negotiator(name: str) -> type[Negotiator]:
+def create_adapter(adapter_type, negotiator_type, name):
+    return adapter_type(name=name, base=negotiator_type(name=name))
+
+
+def get_negotiator(name: str) -> type[Negotiator] | Callable[[str], Negotiator]:
+    if "/" in name:
+        adapter_name, _, negotiator_name = name.partition("/")
+        adapter_type = get_adapter(adapter_name)
+        negotiator_type = get_negotiator(negotiator_name)
+        return functools.partial(create_adapter, adapter_type, negotiator_type)
     if "." not in name:
         if "_" in name:
             name = titlecase(camel_case(name))
-        if not name.endswith("Negotiator"):
-            name = f"{name}Negotiator"
-        name = f"negmas.{name}"
+        try:
+            return get_class(f"negmas.{name}")
+        except:
+            if not name.endswith("Negotiator"):
+                name = f"{name}Negotiator"
+            name = f"negmas.{name}"
+    return get_class(name)
+
+
+def get_adapter(
+    name: str, base_name="NegotiatorAdapter"
+) -> type[Negotiator] | Callable[[str], Negotiator]:
+    if "." not in name:
+        if "_" in name:
+            name = titlecase(camel_case(name))
+        if not name.endswith(base_name):
+            name = f"{name}{base_name}"
+        name = f"negmas.gb.adapters.tau.{name}"
     return get_class(name)
 
 
@@ -97,13 +123,14 @@ def run(
         "--negotiator",
         "-n",
         "-a",
-        help="Negotiator (agent) type",
+        help="Negotiator (agent) type. To use an adapter type, put the adapter name first separated from the negotiator name by a slash (e.g. TAUAdapter/AspirationNegotiator)",
     ),
     reserved: list[float] = typer.Option(None, "--reserved", "-r"),
     normalize: bool = True,
     steps: int = typer.Option(None, "--steps", "-s"),  # type: ignore
     timelimit: int = typer.Option(None, "--time", "--timelimit", "-t"),  # type: ignore
     plot: bool = True,
+    plot_path: Path = None,  # type: ignore
     verbose: bool = typer.Option(False, "--verbose", "-v"),
     progress: bool = False,
     plot_backend: str = "",
@@ -111,6 +138,19 @@ def run(
     history: bool = False,
     stats: bool = True,
     discount: bool = True,
+    only2d: bool = True,
+    show_agreement: bool = False,
+    show_pareto_distance: bool = True,
+    show_nash_distance: bool = True,
+    show_kalai_distance: bool = True,
+    show_max_welfare_distance: bool = True,
+    show_max_relative_welfare_distance: bool = False,
+    show_end_reason: bool = True,
+    show_annotations: bool = False,
+    show_reserved: bool = True,
+    show_total_time: bool = True,
+    show_relative_time: bool = True,
+    show_n_steps: bool = True,
 ):
     if reserved:
         assert len(reserved) == len(negotiators), f"{reserved=} but {negotiators=}"
@@ -139,7 +179,17 @@ def run(
     agents = [
         get_negotiator(_)(name=name) for _, name in zip(negotiators, negotiator_names)
     ]
+    if len(agents) < 2:
+        print(
+            f"At least 2 negotiators are needed: found {[_.__class__.__name__ for _ in agents]}"
+        )
+        return
     session = scenario.make_session(agents, n_steps=steps, time_limit=timelimit)
+    if len(session.negotiators) < 2:
+        print(
+            f"At least 2 negotiators are needed: Only the following could join {[_.__class__.__name__ for _ in session.negotiators]}"
+        )
+        return
     if verbose:
         print(f"Scenario: {domain}")
         print(
@@ -243,11 +293,31 @@ def run(
             print(session.full_trace)  # type: ignore full_trace is defined for SAO and GBM
         else:
             print(session.history)
+    if plot_path:
+        plot_path = Path(plot_path).absolute()
+        plot_path.parent.mkdir(parents=True, exist_ok=True)
     if plot:
         if plot_backend:
             matplotlib.use(plot_backend)
             matplotlib.interactive(plot_interactive)
-        session.plot()
+        session.plot(
+            save_fig=plot_path is not None,
+            path=plot_path.parent if plot_path else None,
+            fig_name=plot_path.name if plot_path else None,
+            only2d=only2d,
+            show_agreement=show_agreement,
+            show_pareto_distance=show_pareto_distance,
+            show_nash_distance=show_nash_distance,
+            show_kalai_distance=show_kalai_distance,
+            show_max_welfare_distance=show_max_welfare_distance,
+            show_max_relative_welfare_distance=show_max_relative_welfare_distance,
+            show_end_reason=show_end_reason,
+            show_annotations=show_annotations,
+            show_reserved=show_reserved,
+            show_total_time=show_total_time,
+            show_relative_time=show_relative_time,
+            show_n_steps=show_n_steps,
+        )
         mng = plt.get_current_fig_manager()
         mng.resize(1024, 860)
         mng.full_screen_toggle()
