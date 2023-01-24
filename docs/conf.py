4d8ba63a6deebede68b5c6b019f309960420e922
Yasser Mohammad <yasserfarouk@gmail.com>
2023-01-24 20:29:31 +0900

    bumpversion (preparing for SCML 2023)

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,7 @@ author = "Yasser Mohammad"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.9.6"
+version = "0.9.7"
 # The full version, including alpha/cost/rc tags.
 release = version
 
