066df8fe4dd45ac55421986a83a8392ba9584e71
Yasser Mohammad <yasserfarouk@gmail.com>
2023-03-08 15:19:03 +0900

    Adding probabilistic negotiation ending
    - Pass pend > 0 to any mechanism to end the negotiation at any step with
      probability pend
    - Pass pend_per_second to any mechanism to end the negotiation at any
      second with probability pend_per_second (extensions are always in
      complete seconds).
    - Added expected_relative_time which may go greater than 1.0
    - Added expected_remaining_time and expected_remaining_steps that take
      pend, pend_per_second into account
    - relative_time, remaining_time, remaining_steps take pend,
      pend_per_second into account ONLY if no time_limit  or n_steps limit
      is given (i.e. time_limit is inf or None and n_steps is None)

--- tests/core/test_sao.py
+++ tests/core/test_sao.py
@@ -6,6 +6,7 @@ from pytest import mark
 
 import negmas
 from negmas import all_negotiator_types
+from negmas.gb.negotiators.timebased import AspirationNegotiator
 from negmas.outcomes import Issue, make_issue
 from negmas.preferences import LinearAdditiveUtilityFunction
 from negmas.sao import EndImmediately, NoneOfferingPolicy, RejectAlways, SAOMechanism
@@ -51,3 +52,58 @@ def test_has_correct_type_name(factory, name, short_name):
     x = factory()
     assert x.type_name == name
     assert x.short_type_name == short_name
+
+
+def test_pend_works():
+    issues: list[Issue] = [
+        make_issue(10, "price"),
+        make_issue(10, "quantity"),
+        make_issue(["red", "green", "blue"], "color"),
+    ]
+    ufuns = [
+        LinearAdditiveUtilityFunction.random(issues=issues, reserved_value=0.0),
+        LinearAdditiveUtilityFunction.random(issues=issues, reserved_value=0.0),
+    ]
+    n = 1000
+    session = SAOMechanism(n_steps=None, time_limit=None, pend=1 / n, issues=issues)
+    for u in ufuns:
+        session.add(AspirationNegotiator(), preferences=u)  # type: ignore
+
+    assert abs(session.expected_relative_time - (1 / (n + 1))) < 1e-8
+    assert session.expected_remaining_time is None
+    assert session.expected_remaining_steps == n
+    assert abs(session.relative_time - (1 / (n + 1))) < 1e-8
+    assert session.remaining_steps is None
+    assert session.remaining_time is None
+    assert session.run().agreement is not None
+    assert session.state.step <= 100 * n
+
+
+def test_pend_per_second_works():
+    issues: list[Issue] = [
+        make_issue(10, "price"),
+        make_issue(10, "quantity"),
+        make_issue(["red", "green", "blue"], "color"),
+    ]
+    ufuns = [
+        LinearAdditiveUtilityFunction.random(issues=issues, reserved_value=0.0),
+        LinearAdditiveUtilityFunction.random(issues=issues, reserved_value=0.0),
+    ]
+    n = 10
+    session = SAOMechanism(
+        n_steps=None, time_limit=None, pend_per_second=1 / n, issues=issues
+    )
+    for u in ufuns:
+        session.add(AspirationNegotiator(), preferences=u)  # type: ignore
+
+    assert session.expected_relative_time < 1e-8
+    assert (
+        session.expected_remaining_time is not None
+        and abs(session.expected_remaining_time - n) < 1e-8
+    )
+    assert session.expected_remaining_steps is None
+    assert session.relative_time < 1e-8
+    assert session.remaining_steps is None
+    assert session.remaining_time is None
+    session.run()
+    assert session.state.time <= 100 * n
