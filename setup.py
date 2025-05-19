9f3bdfaf5a90e44173dc9b209d6ee7fa5f3868cf
Yasser Mohammad <yasserfarouk@gmail.com>
2025-05-19 02:17:56 -0400

    v0.11.5
    Avoiding a twine version issue

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="negmas",
+    version="0.11.4",
+    description="NEGotiations Managed by Agent Simulations",
+    author="Yasser Mohammad",
+    author_email="yasserfarouk@gmail.com",
+    packages=find_packages(),
+)
