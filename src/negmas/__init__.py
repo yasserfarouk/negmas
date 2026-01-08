57efeff3eac4027048bfc9ba80b9549b2a212600
Yasser Mohammad <yasserfarouk@gmail.com>
2026-01-08 09:21:39 +0900

    perf: implement __getattr__ for lazy genius module loading

--- src/negmas/__init__.py
+++ src/negmas/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """A framework for conducting multi-strand multilateral asynchronous negotiations on multiple issues."""
 
-
 __author__ = """Yasser Mohammad"""
 __email__ = "yasserfarouk@gmail.com"
 __version__ = "0.11.4"
@@ -19,7 +18,36 @@ from .sao import *
 from .situated import *
 from .st import *
 from .preferences import *
-from .genius import *
+
+# Genius module exports - lazily loaded via __getattr__
+_GENIUS_EXPORTS = frozenset(
+    {
+        "DEFAULT_JAVA_PORT",
+        "DEFAULT_PYTHON_PORT",
+        "DEFAULT_GENIUS_NEGOTIATOR_TIMEOUT",
+        "ANY_JAVA_PORT",
+        "RANDOM_JAVA_PORT",
+        "get_free_tcp_port",
+        "GeniusBridge",
+        "init_genius_bridge",
+        "genius_bridge_is_running",
+        "genius_bridge_is_installed",
+        "GeniusNegotiator",
+    }
+)
+
+
+def __getattr__(name: str):
+    """Lazy load genius module exports on first access."""
+    if name in _GENIUS_EXPORTS:
+        from . import genius
+
+        return getattr(genius, name)
+    if name == "genius":
+        from . import genius
+
+        return genius
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
 
 
 __all__ = (
@@ -34,8 +62,9 @@ __all__ = (
     + sao.__all__
     + st.__all__
     + inout.__all__
-    + genius.__all__
     + situated.__all__
+    + list(_GENIUS_EXPORTS)
+    + ["genius"]
     # + modeling.__all__
     # + helpers.prob.__all__
     # + [
