a97b54581879423ec5195053f51963f400515880
Yasser Mohammad <yasserfarouk@gmail.com>
2026-01-22 20:31:48 +0900

    Fix ContiguousIssue XML serialization to use efficient integer format
    - Changed _to_xml_str() to use etype='integer' with lowerbound/upperbound attributes
    - Avoids expanding large ranges (e.g., 1-1,000,000) into millions of discrete items
    - Ensures ContiguousIssue round-trips correctly through XML (maintains type consistency)
    - Fixes GeniusBridge test failures (all 136 tests now pass)
    - Compatible with existing Genius XML files that already use this format

--- src/negmas/outcomes/contiguous_issue.py
+++ src/negmas/outcomes/contiguous_issue.py
@@ -46,16 +46,10 @@ class ContiguousIssue(RangeIssue, DiscreteIssue):
         self._n_values = vt[1] - vt[0] + 1  # type: ignore
 
     def _to_xml_str(self, indx):
-        # return (
-        #     f'    <issue etype="integer" index="{indx + 1}" name="{self.name}" type="integer" vtype="integer"'
-        #     f' lowerbound="{self._values[0]}" upperbound="{self._values[1]}" />\n'
-        # )
-
-        output = f'    <issue etype="discrete" index="{indx + 1}" name="{self.name}" type="discrete" vtype="integer">\n'
-        for i, v in enumerate(range(self._values[0], self._values[1] + 1)):
-            output += f'        <item index="{i + 1}" value="{v}" cost="0" description="{v}">\n        </item>\n'
-        output += "    </issue>\n"
-        return output
+        return (
+            f'    <issue etype="integer" index="{indx + 1}" name="{self.name}" type="integer" vtype="integer"'
+            f' lowerbound="{self._values[0]}" upperbound="{self._values[1]}" />\n'
+        )
 
     @property
     def all(self) -> Generator[int, None, None]:
